55ff4e4ca1f1966379c2856a2050af4d5d4e3d24
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-09-27 17:34:46 +0530

    Reset password and set new password routes were added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { SignInForm } from './Layouts/Authentication/SignInForm';
 import { SignUpForm } from './Layouts/Authentication/SignUpForm';
 import { ProfilePage } from './Layouts/Profile/ProfilePage';
 import { AccessDenied } from './Utils/AccessDenied';
+import { ResetPassword } from './Layouts/Reset Password/ResetPassword';
+import { SetNewPassword } from './Layouts/Reset Password/SetNewPassword';
 
 function App() {
   return (
@@ -21,6 +23,10 @@ function App() {
         <Route path="/profile/:username" element={<ProfilePage />} />
         <Route path="/access-denied" element={<AccessDenied />} />
         {/* <Route path='/*' element={<Error404 />} /> */}
+        {/* Others */}
+        <Route path="/reset" element={<ResetPassword />} />
+        <Route path="/set-password/:username" element={<SetNewPassword />} />
+
       </Routes>
     </Router>
     // </div>
