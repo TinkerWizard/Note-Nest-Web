9e89393fa3784fcfbfb85011d5e432559c800691
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-09-27 17:31:12 +0530

    Route for updating password was implemented

--- backend/Routes/Users.ts
+++ backend/Routes/Users.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
-import User from '../Models/User';
-import { authenticateJWT } from '../Middleware/AuthMiddleware';
-
+import express, { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import User from "../Models/User";
+import { authenticateJWT } from "../Middleware/AuthMiddleware";
 
 const router = express.Router();
 interface User {
@@ -13,33 +12,65 @@ interface User {
 }
 
 // Get all users
-router.get('/', async (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
   try {
     const users = await User.find(); // Fetch all users
-    res.json(users);                // Send users as JSON response
+    res.json(users); // Send users as JSON response
   } catch (err: any) {
-    res.status(500).json({ message: 'Error retrieving users', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error retrieving users", error: err.message });
   }
 });
 
+router.get(
+  "/profile/:username",
+  authenticateJWT,
+  async (req: Request, res: Response) => {
+    try {
+      const { username } = req.params;
 
-router.get('/profile/:username', authenticateJWT,  async (req: Request, res: Response) => {
-  try {
-    const { username } = req.params;
+      // Use findOne to get a single user document
+      const user = await User.findOne({ username });
 
-    // Use findOne to get a single user document
-    const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
 
-    if (!user) {
-      return res.status(400).json({ message: 'User not found' });
+      // Send user details in the response
+      res
+        .status(200)
+        .json({ username: user.username, name: user.name, email: user.email });
+    } catch (error: any) {
+      res.status(400).json({ message: `Unexpected error: ${error.message}` });
     }
+  }
+);
 
-    // Send user details in the response
-    res.status(200).json({ username: user.username, name: user.name, email: user.email });
+router.put("/password", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const userExist = await User.updateOne(
+      { username: username }, // Query to find the user
+      { $set: { password: hashedPassword } } // Set the new hashed password
+    );
+    if (userExist.modifiedCount > 0) {
+      return res
+        .status(200)
+        .json({ success: true, message: "Password updated successfully" });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "User not found or password not updated",
+      });
+    }
   } catch (error: any) {
-    res.status(400).json({ message: `Unexpected error: ${error.message}` });
+    console.error("Error updating password:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while updating password",
+    });
   }
 });
-
-
 export default router;
